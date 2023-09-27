19948148036c396d014658b98019f69b97d90650
javaladez90 <javaladez90@aol.com>
2023-09-27 04:51:05 +0000

    Need to fix conversions

--- src/BMICalculator.js
+++ src/BMICalculator.js
@@ -5,10 +5,19 @@ function BMICalculator() {
     const [weight, setWeight] = useState('');
     const [height, setHeight] = useState('');
     const [bmi, setBMI] = useState(null);
+    const [unit, setUnit] = useState('kg-m');
+
+    const handleUnitChange = (e) => {
+        setUnit(e.target.value);
+        const weightValue = e.target.value === 'kg-m' ? weight * 2.20462 : weight / 2.20462;
+        const heightValue = e.target.value === 'kg-m' ? height / 39.3701 : height * 39.3701;
+        setWeight(weightValue);
+        setHeight(heightValue);
+    };
 
     const calculateBMI = () => {
-        const weightKg = parseFloat(weight);
-        const heightM = parseFloat(height);
+        const weightKg = unit === 'lbs' ? parseFloat(weight) * 0.453592 : parseFloat(weight);
+        const heightM = unit === 'inches' ? parseFloat(height) * 0.0254 : parseFloat(height);
         if (weightKg > 0 && heightM > 0) {
             const bmiValue = weightKg / (heightM * heightM);
             setBMI(bmiValue.toFixed(2));
@@ -26,6 +35,10 @@ function BMICalculator() {
                     value={weight}
                     onChange={(e) => setWeight(e.target.value)}
                     />
+                    <select className='weightUnit' value={unit} onChange={handleUnitChange}>
+                        <option value='kg-m'>kg</option>
+                        <option value='lbs-inches'>lbs</option>
+                    </select>
                 <label htmlFor='height'>Height (m):</label>
                 <input
                     type='text'
@@ -33,6 +46,10 @@ function BMICalculator() {
                     value={height}
                     onChange={(e) => setHeight(e.target.value)}
                     />
+                    <select className='weightUnit' value={unit} onChange={handleUnitChange}>
+                        <option value='kg-m'>m</option>
+                        <option value='lbs-inches'>inches</option>
+                    </select>
             </div>
             <button onClick={calculateBMI}>Calculate BMI</button>
             {bmi && (
