fd4f810669f3eb391f6d7f3616862acf299e25dc
javaladez90 <javaladez90@aol.com>
2023-10-02 01:51:01 +0000

    code successfully converts both metric /USA units

--- src/BMICalculator.js
+++ src/BMICalculator.js
@@ -5,64 +5,81 @@ function BMICalculator() {
     const [weight, setWeight] = useState('');
     const [height, setHeight] = useState('');
     const [bmi, setBMI] = useState(null);
-    const [unit, setUnit] = useState('kg-m');
+    const [weightUnit, setWeightUnit] = useState('kg');
+    const [heightUnit, setHeightUnit] = useState('m')
+    
 
-    const handleUnitChange = (e) => {
-       setUnit(e.target.value);
-       let weightValue = weight;
-       let heightValue = height;
-
-       if (e.target.value === 'kg-m') {
-        //convert from lbs to kg
-        weightValue = weight / 2.20462;
-        //convert from inches to meters
-        heightValue = height / 39.3701;
-       } else if (e.target.value === 'lbs-inches') {
-        //convert from kg to lbs
-        weightValue = weight * 2.20462;
-        //convert from meters to inches
-        heightValue = height * 39.3701;
-       }
-
-       setWeight(weightValue);
-       setHeight(heightValue);
+    const handleWeightChange = (e) => {
+       setWeight(e.target.value);
     };
 
+    const handleHeightChange = (e) => {
+        setHeight(e.target.value);
+    };
+
+    const handleWeightUnitChange = (e) => {
+        const newUnit = e.target.value;
+        setWeightUnit(newUnit);
+    };
+
+    const handleHeightUnitChange = (e) => {
+        const newUnit = e.target.value;
+        setHeightUnit(newUnit);
+    };
+       
     const calculateBMI = () => {
-        const weightKg = unit === 'kg-m' ? parseFloat(weight) : parseFloat(weight) * 0.453592 ;
-        const heightM = unit === 'm' ? parseFloat(height) : parseFloat(height) * 0.0254;
+        let weightKg = parseFloat(weight);
+        let heightM = parseFloat(height);
+
+        if (weightUnit === 'lbs') {
+            //convert lbs to kg
+            weightKg = weightKg * 0.453592
+        }
+
+        if (heightUnit === 'inches') {
+            //convert inches to meters
+            heightM = heightM * 0.0254;
+        }
+
         if (weightKg > 0 && heightM > 0) {
             const bmiValue = weightKg / (heightM * heightM);
             setBMI(bmiValue.toFixed(2));
         }
     };
-}
+
+    function interpretBMI(bmi) {
+        if (bmi < 18.5) return 'Underweight';
+        if (bmi < 24.9) return 'Normal Weight';
+        if (bmi < 29.9) return 'Overweight';
+        return 'Obese'
+    }
+
 
     return (
         <div className='bmi-calculator'>
             <h1>BMI Calculator</h1>
             <div>
-                <label htmlFor='weight'>Weight (kg):</label>
+                <label htmlFor='weight'>Weight:</label>
                 <input
                     type='text'
                     id='weight'
                     value={weight}
-                    onChange={(e) => setWeight(e.target.value)}
+                    onChange={handleWeightChange}
                     />
-                    <select className='weightUnit' value={unit} onChange={handleUnitChange}>
-                        <option value='kg-m'>kg</option>
-                        <option value='lbs-inches'>lbs</option>
+                    <select className='weightUnit' value={weightUnit} onChange={handleWeightUnitChange}>
+                        <option value='kg'>kg</option>
+                        <option value='lbs'>lbs</option>
                     </select>
-                <label htmlFor='height'>Height (m):</label>
+                <label htmlFor='height'>Height:</label>
                 <input
                     type='text'
                     id='height'
                     value={height}
-                    onChange={(e) => setHeight(e.target.value)}
+                    onChange={handleHeightChange}
                     />
-                    <select className='weightUnit' value={unit} onChange={handleUnitChange}>
-                        <option value='kg-m'>m</option>
-                        <option value='lbs-inches'>inches</option>
+                    <select className='heightUnit' value={heightUnit} onChange={handleHeightUnitChange}>
+                        <option value='m'>m</option>
+                        <option value='inches'>inches</option>
                     </select>
             </div>
             <button onClick={calculateBMI}>Calculate BMI</button>
@@ -74,13 +91,8 @@ function BMICalculator() {
             )}
         </div>
     );
+            }
 
 
-function interpretBMI(bmi) {
-    if (bmi < 18.5) return 'Underweight';
-    if (bmi < 24.9) return 'Normal Weight';
-    if (bmi < 29.9) return 'Overweight';
-    return 'Obese'
-}
 
-export default BMICalculator
+export default BMICalculator;
